61d84a29f4f13af61fab01e1e1febce940ad6c61
Precious Akams <preciousakams@yahoo.com>
2022-01-26 23:31:43 +0100

    store update

---
+++ js/validations.js
@@ -0,0 +1,68 @@
+// show a message with a type of the input
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#reservation");
+
+const NAME_REQUIRED = "Name is required";
+const EMAIL_REQUIRED = "Email is required";
+const EMAIL_INVALID = "Please enter a correct email address";
+const DATE_TIME_REQUIRED = "Please enter your date and time";
+const NO_OF_PEOPLE_REQUIRED = "Please indicate number of people";
+
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+	// validate the form
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+    let dateTimeValid = hasValue(form.elements["datetime"], DATE_TIME_REQUIRED);
+    let No_PeopleValid = hasValue(form.elements["no_of_people"], NO_OF_PEOPLE_REQUIRED);
+    
+	// if valid, submit the form.
+	if (nameValid && emailValid  && dateTimeValid && No_PeopleValid) {
+		
+            $('#myModal').modal('show');
+            e.preventDefault();
+	}
+});
+
+
